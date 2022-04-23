f2c3bf35534cc1b80f0c4a479c6a816aa766679b
Diego Rivera <rivmont.diego@gmail.com>
2022-04-23 18:10:58 -0400

    Week 1 first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+        label: "Week 1 notes",
+        url: "week1/index.html"
+    }
+
+]
+
+function loadAssignments() {
+    var ol = document.getElementById('assignmentsList');
+    for (let i = 0; i < links.length; i++){
+        var li = document.createElement('li');
+        var a = document.createElement('a');
+        a.href=links[i].url;
+        a.innerText=links[i].label;
+        li.appendChild(a);
+        ol.appendChild(li);    
+    }
+}
