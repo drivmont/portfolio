db553b1bed8a8a5d0d93e950d7631fac6d18d225
Diego Rivera <rivmont.diego@gmail.com>
2022-07-19 16:50:34 -0400

    Memory issue

--- week6/main.js
+++ week6/main.js
@@ -11,8 +11,9 @@ const view = {
     tasks: document.getElementById('tasks'),
     filters: document.getElementById('filters')
 }
-let toDoList = JSON.parse(window.localStorage.getItem('tasks'));
+
 loadMemory();
+let toDoList = JSON.parse(window.localStorage.getItem('tasks'));
 loadTasks(toDoList,taskViewer);
 view.addTask.addEventListener('click', () => addTask(view.newTask.value,toDoList,taskViewer), false);
 view.tasks.addEventListener('click',(event) => changeTask(event,toDoList,taskViewer), false);
